c1c83fa84c86c97bc87f95b579a4eceed7f3b352
Jacob <jacobeljones56@gmail.com>
2022-01-13 18:09:10 -0500

    Edit added

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -61,6 +62,37 @@ namespace RestaurantRaterMVC.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // Get
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
 
     }
 }
