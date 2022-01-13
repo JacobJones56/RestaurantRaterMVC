c8a330c85671a6d83ebfc6a2840154b54729feef
Jacob <jacobeljones56@gmail.com>
2022-01-13 18:19:54 -0500

    Details and small edit fix for no AntiForgeryToken

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -93,6 +93,23 @@ namespace RestaurantRaterMVC.Controllers
             return View(restaurant);
         }
 
+        // Get 
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+
+
 
     }
 }
